c84e00fc69bf5bb3b38543196e57ed793f8befc0
mfrony2003 <mfrony2003@gmail.com>
2021-10-25 20:35:42 +0600

    git hub context

--- Docker_NodeAPI/app.js
+++ Docker_NodeAPI/app.js
@@ -2,12 +2,13 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
+
 const port = 3001
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!12')
 })
 
 

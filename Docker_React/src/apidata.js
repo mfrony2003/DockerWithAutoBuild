36656f315713d8f7f285879206b5318b27ded6bf
mfrony2003 <mfrony2003@gmail.com>
2021-11-11 01:48:51 +0600

    172.19.0.

--- Docker_React/src/apidata.js
+++ Docker_React/src/apidata.js
@@ -14,7 +14,7 @@ class ApiComponent extends React.Component {
     const http = require('http');
 
     var options = {
-        host: '172.19.0.3',
+        host: '172.19.0.2',
         port: 3001,
         path: '/person/names',
         method: 'Get'

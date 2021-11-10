3439617dc172c21873cf4a6a1007187fca151181
mfrony2003 <mfrony2003@gmail.com>
2021-11-10 22:36:16 +0600

    change compose file

--- Docker_React/src/apidata.js
+++ Docker_React/src/apidata.js
@@ -11,32 +11,6 @@ class ApiComponent extends React.Component {
     }
     
   componentDidMount() {
-    // const https = require('https');
-    // var options = {
-    //     host: myapi,
-    //     port: 3001,
-    //     path: '/person/names',
-    //     method: 'GET'
-    //   };
-      
-      
-    //   const req = https.request(options, res => {
-    //     console.log(`statusCode: ${res.statusCode}`);
-      
-    //     res.on('data', d => {
-    //         var json = d.json();
-    //         this.setState({
-    //             items: json,
-    //             DataisLoaded: true
-    //         });
-    //      // process.stdout.write(d);
-    //     });
-    //   });
-      
-    //   req.on('error', error => {
-    //     console.error(error);
-    //   });
-
       
     const apiUrl = 'http://localhost:3001/person/names';
     fetch(apiUrl)
